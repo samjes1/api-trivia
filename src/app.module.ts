e404bce9066fd60f04f4efc1842affb257e7bec3
Samys Molina <jetkei951@gmail.com>
2025-07-12 08:31:33 -0400

    add nestjs/config and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { QuestionsModule } from './questions/questions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
 
 
 @Module({
-  imports: [QuestionsModule],
+  imports: [  ConfigModule.forRoot({ isGlobal: true }), // configuracion del .env 
+     TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',    
+      password: '123456789', 
+      database: 'trivia_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,          
+      //logging: true,              // Opcional: ver queries en consola
+    }),
+    inject: [ConfigService],
+      }),
+    QuestionsModule,
+    CategoryModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT ?? 3000 // se debe usar el valor real del puerto. 
+  await app.listen(port);
+  Logger.log(`Conection in port ${port}`)
 }
 bootstrap();
